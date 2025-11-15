19a5338f21ef5d68b8fbda44b8758c3ff0ea1d9d
brendi <bmchoque@fi.uba.ar>
2025-11-15 19:32:04 -0300

    Modifico funcionalidad de verificar nombre

--- frontend/profile-editar.js
+++ frontend/profile-editar.js
@@ -3,23 +3,43 @@ const nombreInput = document.getElementById('nombre-input');
 const iconoValidacion = document.getElementById('icon-validacion'); 
 const mensajeValidacion = document.getElementById('mensaje-validacion');
 
-mensajeValidacion.textContent= '';
+mensajeValidacion.textContent = '';
 iconoValidacion.innerHTML = '';
 nombreInput.classList.remove('is-success', 'is-danger');
 
-async function verificarNombre(nombre) {
-  if (nombre.length < 4) {
-    nombreInput.classList.remove('is-success');
-    nombreInput.classList.add('is-danger')
-    mensajeValidacion.textContent = 'El nombre es muy corto';
-    mensajeValidacion.className = 'help is-danger';
-    iconoValidacion.innerHTML = '<i class="fa-solid fa-skull"></i>';
+function verificarNombre(nombre) {
+  if (nombre.length === 0) {
+    nombreInput.classList.remove('is-success', 'is-danger');
+    iconoValidacion.innerHTML = '';
+    mensajeValidacion.textContent = '';
+    mensajeValidacion.className = 'help';
     return;
   }
-
-  // Mostrar loading
-  iconoValidacion.innerHTML = '<i class="fas fa-spinner fa-pulse"></i>';
+  
+  if (nombre.length < 4) {
+    nombreInput.classList.remove('is-success');
+    nombreInput.classList.add('is-danger');
+    mensajeValidacion.textContent = 'El nombre es muy corto (mínimo 4 letras)';
+    mensajeValidacion.className = 'help is-danger';
+    iconoValidacion.innerHTML = '<i class="fa-solid fa-skull"></i>';
+  } 
+  else {
+    nombreInput.classList.remove('is-danger');
+    nombreInput.classList.add('is-success');
+    iconoValidacion.innerHTML = '<i class="fas fa-check"></i>';
+    mensajeValidacion.className = 'help is-success';
+    mensajeValidacion.textContent = 'Nombre válido :)';
+  }
 }
+
+let temporizador; 
+nombreInput.addEventListener('input', function() {
+  clearTimeout(temporizador);
+  
+  temporizador = setTimeout(() => {
+    verificarNombre(this.value);
+  }, 300);
+});
 
 
 // Validacion de la Contraseña
