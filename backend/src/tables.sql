5b37a823e6215fed7a614ec320f57c92fe251326
ari <arperezo@fi.uba.ar>
2025-12-09 13:51:00 -0300

    Modifico tablas e inserts

--- backend/src/tables.sql
+++ backend/src/tables.sql
@@ -12,27 +12,48 @@ CREATE TABLE IF NOT EXISTS posts (
     user_id INT NOT NULL,
     content VARCHAR(255) NOT NULL,
     image VARCHAR(255), 
+    lunas INT DEFAULT 0,
+    category_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
+
 CREATE TABLE IF NOT EXISTS categories (
-    FOREIGN KEY (post_id) REFERENCES cat_post(post_id),
-    FOREIGN KEY (user_id) REFERENCES cat_users(user_id)
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
 );
+
+
+
+
+
 
 -- Insert inicial
 INSERT INTO users (username, password, bio, pfp)
-SELECT 'arito', '1234', 'always sleeping', 'https://i.pinimg.com/736x/e6/a2/39/e6a239754826cc9ea0000eaf4c72cc02.jpg'
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'arito');
+SELECT 'ari-dreamlog', '1234', 'im always sleeping Zzz', 'https://i.pinimg.com/736x/e6/a2/39/e6a239754826cc9ea0000eaf4c72cc02.jpg'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'ari-dreamlog');
+
+INSERT INTO users (username, password, bio, pfp)
+SELECT 'maga-dreamlog', '1234', '<3', 'https://ovicio.com.br/wp-content/uploads/2024/11/20241118-arcane-temporada-2-ovicio-1-555x555.webp'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'maga-dreamlog');
+
+INSERT INTO users (username, password, bio, pfp)
+SELECT 'bren-dreamlog', '1234', 'tengo que dormir 8 horas', 'https://i.pinimg.com/736x/c9/e4/41/c9e4411b4ae20d9a6e8abeb7f893a633.jpg'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'bren-dreamlog');
+
+
 
 -- Insert inicial del post solo si no existe
-INSERT INTO posts (user_id, content)
-SELECT 
-    (SELECT user_id FROM users WHERE username = 'arito'),
-    'Mi primer sueño en DreamLog 🌙'
-WHERE NOT EXISTS (
-    SELECT 1 FROM posts 
-    WHERE user_id = (SELECT user_id FROM users WHERE username = 'arito')
-    AND content = 'soñe con un apocalipsis zombie y no sobreviví la primera noche'
-);
+INSERT INTO posts (user_id, content, image)
+VALUES (1, 'sigo soñando con ascensores wtf', NULL);
+
+INSERT INTO posts (user_id, content, image)
+VALUES (2, 'soñe que estaba dentro de un videojuego y perdia mis tres vidas', NULL);
+
+INSERT INTO posts (user_id, content, image)
+VALUES (3, 'soñe que despertaba pero seguia durmiendo, esa cosa me seguia persiguiendo', NULL);
+
+
+
