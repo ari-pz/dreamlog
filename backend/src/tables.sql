51ad9f9dfba579f7ddb587ea0729b19b6f8f07ec
ari-pz <arperezo@fi.uba.ar>
2025-12-09 14:01:50 -0300

    Update tables.sql

--- backend/src/tables.sql
+++ backend/src/tables.sql
@@ -31,29 +31,18 @@ CREATE TABLE IF NOT EXISTS categories (
 
 
 -- Insert inicial
-INSERT INTO users (username, password, bio, pfp)
-SELECT 'ari-dreamlog', '1234', 'im always sleeping Zzz', 'https://i.pinimg.com/736x/e6/a2/39/e6a239754826cc9ea0000eaf4c72cc02.jpg'
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'ari-dreamlog');
-
-INSERT INTO users (username, password, bio, pfp)
-SELECT 'maga-dreamlog', '1234', '<3', 'https://ovicio.com.br/wp-content/uploads/2024/11/20241118-arcane-temporada-2-ovicio-1-555x555.webp'
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'maga-dreamlog');
-
-INSERT INTO users (username, password, bio, pfp)
-SELECT 'bren-dreamlog', '1234', 'tengo que dormir 8 horas', 'https://i.pinimg.com/736x/c9/e4/41/c9e4411b4ae20d9a6e8abeb7f893a633.jpg'
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'bren-dreamlog');
-
+INSERT INTO users (username, password, bio, pfp) VALUES
+('ari-dreamlog',  '1234', 'im always sleeping Zzz', 'https://i.pinimg.com/736x/e6/a2/39/e6a239754826cc9ea0000eaf4c72cc02.jpg'),
+('maga-dreamlog', '1234', '<3', 'https://ovicio.com.br/wp-content/uploads/2024/11/20241118-arcane-temporada-2-ovicio-1-555x555.webp'),
+('bren-dreamlog', '1234', 'tengo que dormir 8 horas','https://i.pinimg.com/736x/c9/e4/41/c9e4411b4ae20d9a6e8abeb7f893a633.jpg');
 
 
 -- Insert inicial del post solo si no existe
-INSERT INTO posts (user_id, content, image)
-VALUES (1, 'sigo soñando con ascensores wtf', NULL);
+INSERT INTO posts (user_id, content, image) VALUES
+(1, 'sigo soñando con ascensores wtf', NULL),
+(2, 'soñe que estaba dentro de un videojuego y perdia mis tres vidas', NULL),
+(3, 'soñe que despertaba pero seguia durmiendo, esa cosa me seguia persiguiendo', NULL);
 
-INSERT INTO posts (user_id, content, image)
-VALUES (2, 'soñe que estaba dentro de un videojuego y perdia mis tres vidas', NULL);
-
-INSERT INTO posts (user_id, content, image)
-VALUES (3, 'soñe que despertaba pero seguia durmiendo, esa cosa me seguia persiguiendo', NULL);
 
 
 
