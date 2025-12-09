49df1af853509f334ccfa429040bac416da3b308
ari-pz <arperezo@fi.uba.ar>
2025-12-09 14:21:57 -0300

    Update tables.sql

--- backend/src/tables.sql
+++ backend/src/tables.sql
@@ -11,37 +11,36 @@ CREATE TABLE IF NOT EXISTS posts (
     post_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     content VARCHAR(255) NOT NULL,
-    image VARCHAR(255), 
-    lunas INT DEFAULT 0,
-    category_id INT,
+    --image VARCHAR(255), 
+    --lunas INT DEFAULT 0,
+    --category_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+    --FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS categories (
-    category_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
-);
+--CREATE TABLE IF NOT EXISTS categories (
+--    category_id SERIAL PRIMARY KEY,
+--    name VARCHAR(50) NOT NULL UNIQUE
+--);
 
+TRUNCATE TABLE posts, users RESTART IDENTITY CASCADE;
 
-
-
-
-
--- Insert inicial
 INSERT INTO users (username, password, bio, pfp) VALUES
-('ari-dreamlog',  '1234', 'im always sleeping Zzz', 'https://i.pinimg.com/736x/e6/a2/39/e6a239754826cc9ea0000eaf4c72cc02.jpg'),
+('ari-dreamlog', '1234', 'im always sleeping Zzz', 'https://i.pinimg.com/736x/e6/a2/39/e6a239754826cc9ea0000eaf4c72cc02.jpg'),
 ('maga-dreamlog', '1234', '<3', 'https://ovicio.com.br/wp-content/uploads/2024/11/20241118-arcane-temporada-2-ovicio-1-555x555.webp'),
-('bren-dreamlog', '1234', 'tengo que dormir 8 horas','https://i.pinimg.com/736x/c9/e4/41/c9e4411b4ae20d9a6e8abeb7f893a633.jpg');
+('bren-dreamlog', '1234', 'tengo que dormir 8 horas', 'https://i.pinimg.com/736x/c9/e4/41/c9e4411b4ae20d9a6e8abeb7f893a633.jpg');
 
-
--- Insert inicial del post solo si no existe
 INSERT INTO posts (user_id, content) VALUES
 (1, 'sigo soñando con ascensores wtf'),
 (2, 'soñe que estaba dentro de un videojuego y perdia mis tres vidas'),
 (3, 'soñe que despertaba pero seguia durmiendo, esa cosa me seguia persiguiendo');
+
+
+
+
+
 
 
 
