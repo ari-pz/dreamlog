ef982851646c1f44068005bb51d8d0bfaab0cd6e
brendi <bmchoque@fi.uba.ar>
2025-12-09 12:50:58 -0300

    MOdifico tablas

--- backend/src/tables.sql
+++ backend/src/tables.sql
@@ -1,7 +1,4 @@
-DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
@@ -10,12 +7,18 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS posts (
     post_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     content VARCHAR(255) NOT NULL,
+    image VARCHAR(255), 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    FOREIGN KEY (post_id) REFERENCES cat_post(post_id),
+    FOREIGN KEY (user_id) REFERENCES cat_users(user_id)
 );
 
 -- Insert inicial
