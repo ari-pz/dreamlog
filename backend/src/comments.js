0d4ee3e988000c9945342e8eb13d0a9ffa278d78
Bren-Ch <bmchoque@fi.uba.ar>
2025-12-11 15:58:22 -0300

    agrego funciones de los comentarios

---
+++ backend/src/comments.js
@@ -0,0 +1,43 @@
+const { Pool } = require("pg");
+const dbClient = new Pool ({
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "dreamlog",
+});
+
+// Obtener comentarios de un post
+async function getCommentsByPostId(post_id) {
+  const query = `
+    SELECT 
+      comments.comment_id,
+      comments.user_id,
+      comments.content,
+      comments.url,
+      comments.created_at,
+      users.username
+    FROM comments
+    INNER JOIN users ON comments.user_id = users.user_id
+    WHERE comments.post_id = $1
+    ORDER BY comments.created_at ASC
+  `;
+  const { rows } = await dbClient.query(query, [post_id]);
+  return rows;
+}
+
+// Crear un comentario
+async function insertComment(user_id, post_id, content, url) {
+  const query = `
+    INSERT INTO comments (user_id, post_id, content, url)
+    VALUES ($1, $2, $3, $4)
+    RETURNING comment_id, created_at
+  `;
+  const { rows } = await dbClient.query(query, [user_id, post_id, content, url]);
+  return rows[0];
+}
+
+module.exports = {
+  getCommentsByPostId,
+  insertComment
+};
