e186cecffc40e477420559e4bd005899101941d8
Bren-Ch <bmchoque@fi.uba.ar>
2025-12-11 09:03:26 -0300

    cambio el icono de comentario

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -59,7 +59,11 @@ async function cargarMisPosts() {
              data-post-id="${post.post_id}"
              style="cursor:pointer;"></i>
           <span class="moon-count" data-post-id="${post.post_id}"></span>
-          <span>0 COMENTARIOS</span>
+          <i class="fa-regular fa-comment comment-icon" 
+            data-post-id="${post.post_id}" 
+            style="cursor:pointer; margin-left: 1rem;"></i>
+          <span class="comment-count" data-post-id="${post.post_id}">${post.comment_count || 0}</span>
+      </div>
         </div>
       `;
 
